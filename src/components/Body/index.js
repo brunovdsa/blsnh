33ccc137607185a536e674db81a536d671371ffb
Bruno Vinícius Souza de Sá <brunovinciussouza.sa@gmail.com>
2021-08-17 15:09:44 -0300

    mobile started

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import background from "../../img/background.jpg";
+
+import GenericButton from '../common/GenericButton';
+import mbackground from "../../img/mbackground.png";
+import About from "../../views/About/index"
+import logo from "../../img/logo.png";
 
 import './styles.css'
 
@@ -7,7 +11,18 @@ class Body extends Component {
   render() {
     return (
         <div>
-          <img className="background" src={background}></img>
+          <section className="main-section">
+            <h1>
+              <a href="/">
+                <img src={logo} id="logo" alt="logo"></img>
+              </a>
+            </h1>
+            <div className="wrapper">
+              <GenericButton></GenericButton>
+            </div>
+            <img className="background" src={mbackground}></img>            
+          </section>
+          <About></About>         
         </div>
     );
   }
---
+++ src/components/common/GenericButton/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './styles.css'
+
+class GenericButton extends Component {
+  render() {
+    return (
+        <div>
+            <button>Get Started!</button>
+        </div>
+    );
+  }
+}
+
+export default GenericButton;
